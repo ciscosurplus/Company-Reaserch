1d413fab3cd657a5ebccc8503a7aef7d03f6b441
Graham C <ciscosurplus@gmail.com>
2025-06-05 12:22:17 +0100

    fix: restore full webhook url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,3 @@
 export const API_CONFIG = {
-  WEBHOOK_URL: 'https://n8.ngnc.co.uk/webhook-test/1a830376-1355-42a5-'
+  WEBHOOK_URL: 'https://n8.ngnc.co.uk/webhook-test/1a830376-1355-42a5-9fc0-fb1b9bc4d1fe'
 } as const;
